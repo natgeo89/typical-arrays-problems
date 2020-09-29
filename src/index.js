e7a1ad66768553940ecca7788b736c0892d4409b
Sakharuk Viktar <natgeogr93@gmail.com>
2020-09-29 20:18:23 +0300

    develop functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0) return 0;
+
+  array.sort((a,b) => a - b);
+  return array[0];
 }
+
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0) return 0;
+
+  array.sort((a,b) => a - b);
+  return array.pop();
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length === 0 || array.length === 0) return 0;
+
+  let sumOfArray = array.reduce((accum, item)=> accum + item ,0)
+  return sumOfArray/array.length;
 }
